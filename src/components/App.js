d805b4bac49374cf85d38ae61c3c898d8891c7bc
LeeJones95 <leonna.jones18@gmail.com>
2021-07-22 23:24:02 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import BlogPost from "./BlogPost";
+import BlogContent from "./BlogContent";
+import Comment from "./Comment";
 
 function App() {
   return <BlogPost />;
